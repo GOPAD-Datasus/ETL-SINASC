59305106af841428e9a8b8bcc5ad39ce11b2dae8
LordRiccard <106322393+LordRiccard@users.noreply.github.com>
2025-04-22 13:39:02 -0300

    feat: updated main.py

--- etl/__main__.py
+++ etl/__main__.py
@@ -1,2 +1,10 @@
+from etl.src.extraction.extract import extract
+from etl.src.transformation.transform import transform
+import json
+
 if __name__ == '__main__':
-    pass
+    with open('../parameters/params.json') as json_file:
+        params = json.load(json_file)
+
+    extract(params)
+    transform(params)
