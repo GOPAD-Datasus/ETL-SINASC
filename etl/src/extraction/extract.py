be38efee85b2c92e9c98e5e49a676631921e2a22
LordRiccard <106322393+morsoletodev@users.noreply.github.com>
2025-04-25 17:45:11 -0300

    refactor: Better organized extract.py

--- etl/src/extraction/extract.py
+++ etl/src/extraction/extract.py
@@ -1,46 +1,38 @@
 import urllib.error
 from urllib.request import urlretrieve
-import os
-import json
+
+from etl.src.extraction.utils import *
 
 
-def load_urls () -> dict:
-    with open('../parameters/extract_urls.json') as json_file:
-        return json.load(json_file)
+def extract(params: dict) -> None:
+    """
+    Main extraction function. Downloads every file inside
+    the interval [starting_year, ending_year].
+    The source urls can be found inside get_urls() from
+    utils
 
-def make_temp_folder () -> str:
-    temp_folder = '../temp'
-    if not os.path.exists(temp_folder):
-        os.mkdir(temp_folder)
-    return temp_folder
+    param:
+        params: dict of parameters
+    """
+    initial_year = params['starting_year']
+    final_year = params['ending_year'] + 1
 
-def check_file_exists (path: str) -> bool:
-    return os.path.isfile(path)
+    dict_ = get_urls()
 
-def get_from_opendatasus (params: dict):
-    inicial_year = params['starting_year']
-    final_year = params['ending_year']
+    make_temp_folder()
+    raw_folder = '../temp/raw'
 
-    dict_ = load_urls()
-    temp_folder = make_temp_folder()
-
-    for i in range(inicial_year, final_year):
-        link = dict_[f'{i}']
-        output_file = f'{temp_folder}/DN{i}.csv'
+    for i in range(initial_year, final_year):
+        source_link = dict_[i]
+        output_file = f'{raw_folder}/DN{i}.csv'
 
         if not check_file_exists(output_file):
             try:
-                urlretrieve(link,
+                urlretrieve(source_link,
                             output_file)
             except urllib.error.URLError:
                 error_ = (f'Url corresponding to year {i}'
-                          f' is wrong, try checking it on'
-                          f' extract_urls.json')
-                raise RuntimeError(error_)
-                # TODO: improve error handling:
-                # to allow continuous download of
-                # other files
-
-def extract(params: dict) -> None:
-    if params['source'] == 'opendatasus':
-        get_from_opendatasus(params)
+                          f' is wrong, try changing on'
+                          f' extraction/utils.py inside'
+                          f' get_urls()')
+                raise RuntimeWarning(error_)
---
+++ etl/src/extraction/utils.py
@@ -0,0 +1,58 @@
+import os
+
+
+def get_urls () -> dict:
+    """
+    Returns a dictionary with links corresponding to each year
+    In case the location of a single or multiple databases
+    change, altering it here can solve the problem.
+
+    returns:
+        dict: Pair year and link for all desirable years
+    """
+    return {
+        2012: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2012.csv',
+        2013: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2013.csv',
+        2014: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2014.csv',
+        2015: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2015.csv',
+        2016: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2016.csv',
+        2017: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2017.csv',
+        2018: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2018.csv',
+        2019: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2019.csv',
+        2020: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2020.csv',
+        2021: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2021.csv',
+        2022: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2022.csv',
+        2023: 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SINASC/csv/SINASC_2023.csv'
+    }
+
+def make_temp_folder () -> None:
+    """
+    Creates the temporary file structure to hold raw and
+    processed files. The structure is defined below:
+
+    + temp: root folder
+      - raw: raw data extracted from source
+      - processed: processed data
+    """
+    temp_folder = '../temp'
+    if not os.path.exists(temp_folder):
+        os.mkdir(temp_folder)
+
+    raw_folder = temp_folder + '/raw'
+    if not os.path.exists(raw_folder):
+        os.mkdir(raw_folder)
+
+    processed_folder = temp_folder + '/processed'
+    if not os.path.exists(processed_folder):
+        os.mkdir(processed_folder)
+
+def check_file_exists (path: str) -> bool:
+    """
+    Returns True if a file exists. False if otherwise.
+
+    param:
+        path (str): Path leading to the file
+    return:
+        bool: State of the file
+    """
+    return os.path.isfile(path)
