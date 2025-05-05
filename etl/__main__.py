67a202e432be5f50cdbdd01217e952d92d2f3efe
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-05-05 14:38:21 -0300

    style: format according to PEP 8

--- etl/__main__.py
+++ etl/__main__.py
@@ -2,6 +2,7 @@ from etl.extraction.extract import extract
 from etl.transformation.transform import transform
 import json
 
+
 if __name__ == '__main__':
     with open('../parameters/params.json') as json_file:
         params = json.load(json_file)
