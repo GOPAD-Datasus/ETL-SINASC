447160dff2cafb2a750a0710d519013715042f5c
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-05-14 17:09:43 -0300

    refactor: rename url to path

--- etl/transformation/year_specific/__init__.py
+++ etl/transformation/year_specific/__init__.py
@@ -4,9 +4,10 @@ import pandas as pd
 import etl.transformation.year_specific.year_2012
 
 
-def apply_year_specific_changes (url: str,
+def apply_year_specific_changes (path: str,
                                  year: int) -> pd.DataFrame:
     package = f'etl.transformation.year_specific.year_{year}'
     handler = getattr(importlib.import_module(package),
                       f'Handler{year}')
-    return handler(url).pipeline()
+
+    return handler(path).pipeline()
