30fdfa8f1a1e3a3fc16457f5a3cb83f4393c671e
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-05-14 17:10:14 -0300

    docs: add comment to apply_year

--- etl/transformation/year_specific/__init__.py
+++ etl/transformation/year_specific/__init__.py
@@ -6,6 +6,15 @@ import etl.transformation.year_specific.year_2012
 
 def apply_year_specific_changes (path: str,
                                  year: int) -> pd.DataFrame:
+    """
+    Calls each year's corresponding .py dynamically
+    using getattr, simplifying the inclusion of more
+    years
+
+    param:
+        path (str): Path to the .csv file
+        year (int): File's corresponding year
+    """
     package = f'etl.transformation.year_specific.year_{year}'
     handler = getattr(importlib.import_module(package),
                       f'Handler{year}')
