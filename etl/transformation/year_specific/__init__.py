1836c13a3897c6ac821d209ad4d314393cba4aec
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-05-14 01:26:35 -0300

    fix: broken dotted path

--- etl/transformation/year_specific/__init__.py
+++ etl/transformation/year_specific/__init__.py
@@ -6,7 +6,7 @@ import etl.transformation.year_specific.year_2012
 
 def apply_year_specific_changes (url: str,
                                  year: int) -> pd.DataFrame:
-    package = f'etl.src.transformation.year_specific.year_{year}'
+    package = f'etl.transformation.year_specific.year_{year}'
     handler = getattr(importlib.import_module(package),
                       f'Handler{year}')
     return handler(url).pipeline()
