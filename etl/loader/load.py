252661bcfa7312cb370a1b5235639e14abff95dd
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-08-09 15:45:17 -0300

    refactor: reorganized module for maintainability

--- etl/loader/load.py
+++ etl/loader/load.py
@@ -1,8 +1,19 @@
-from etl.loader.utils import *
+import pandas as pd
+
+from etl.loader.utils import create_db_engine, insert_data
+from utils import load_data
 
 
-def load(params: dict):
+def load():
     url = 'data/processed/'
+    engine = create_db_engine()
+    file_list = load_data(url, endswith='.parquet')
 
-    for item in load_data(url, params):
-        insert_data(pd.read_parquet(item))
+    with engine.connect() as conn:
+        for item in file_list:
+            try:
+                df = pd.read_parquet(item)
+                with conn.begin():
+                    insert_data(conn, df, 'sinasc')
+            except Exception as e:
+                raise Warning(f'Failed to load {item}: {e}')
--- etl/loader/utils.py
+++ etl/loader/utils.py
@@ -1,18 +1,17 @@
 import os
+
+import pandas as pd
+import sqlalchemy.engine
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-import pandas as pd
 
 
-def load_data (url: str, params: dict) -> list:
-
-    return [f'{url}DN{x}.parquet.gzip'
-            for x in range(params['starting_year'],
-                           params['ending_year'] + 1)]
-
-
-def insert_data (df: pd.DataFrame):
+def create_db_engine () -> sqlalchemy.engine.Engine:
     load_dotenv()
+
+    required_env = ['user', 'password', 'host', 'port', 'db']
+    if any(os.getenv(var) is None for var in required_env):
+        raise EnvironmentError("Incomplete .env file")
 
     user = os.getenv('user')
     password = os.getenv('password')
@@ -23,8 +22,14 @@ def insert_data (df: pd.DataFrame):
     engine = create_engine(
         f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}")
 
-    with engine.connect() as conn:
-        with conn.begin():
-            df.to_sql('sinasc',
-                      con=conn,
-                      if_exists="append")
+    return engine
+
+
+def insert_data (conn: sqlalchemy.engine.Connection,
+                 df: pd.DataFrame,
+                 table_name: str):
+    df.to_sql(table_name,
+              con=conn,
+              chunksize=50_000,
+              if_exists="append",
+              method='multi')
