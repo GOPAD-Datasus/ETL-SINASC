7a157c9ce51c228c21676762df64be939fb0c9f3
morsoletodev <106322393+morsoletodev@users.noreply.github.com>
2025-08-10 14:53:30 -0300

    cleanup: remove commented code from transform

--- etl/transformation/transform.py
+++ etl/transformation/transform.py
@@ -19,30 +19,3 @@ def transform() -> None:
             zip(raw_files, pro_files, handlers):
         if not check_file_exists(out_file):
             handle_transformation(in_file, handler, out_file)
-
-
-    # # TODO: Remove necessity
-    # initial_year = params['starting_year']
-    # final_year = params['ending_year'] + 1
-    #
-    # extension = '.parquet.gzip'
-    #
-    #
-    #
-    # # raw_files = load_data(raw_folder, '.csv')
-    #
-    # # pro_files = []
-    # # for file in raw_files:
-    # #     file = str(os.path.basename(file))[:-4]
-    # #     pro_files.append(os.path.join(pro_folder, f'{file}{extension}'))
-    # #
-    # # print(pro_files)
-    #
-    # for year in range(initial_year, final_year):
-    #     input_file = f'{raw_folder}/DN{year}.csv'
-    #     output_file = f'{pro_folder}/DN{year}.parquet.gzip'
-    #
-    #     if check_file_exists (output_file):
-    #         continue
-    #
-    #     handle_year(input_file, year, output_file)
